0a33cce262e89b6e0d1df40d60bf2c3cde06dd5f
Sebastian Werner <s.werner@sebastian-software.de>
2019-06-25 21:37:09 +0200

    Some structural cleanups

--- src/quality.ts
+++ src/quality.ts
@@ -1,6 +1,67 @@
+import restrictedGlobals from "confusing-browser-globals"
+
 import { ESLintRules } from "./types"
 
 export const quality: ESLintRules = {
+  "@typescript-eslint/no-angle-bracket-type-assertion": "error",
+  "@typescript-eslint/no-namespace": "error",
+  "@typescript-eslint/no-array-constructor": "error",
+  "@typescript-eslint/no-useless-constructor": "error",
+
+  // We are fully in TypeScript. PropTypes are not useful anymore.
+  "react/prop-types": "off",
+
+  // These are generally a good idea but do not work well with TypeScript usage
+  "import/export": "off",
+
+  // Conflicts with TypeScript import/export e.g. interfaces
+  "import/named": "off",
+
+  "react-hooks/rules-of-hooks": "error",
+  "react-hooks/exhaustive-deps": "warn",
+
+  // The ESLint browser environment defines all browser globals as valid,
+  // even though most people don't know some of them exist (e.g. `name` or `status`).
+  // This is dangerous as it hides accidentally undefined variables.
+  // We blacklist the globals that we deem potentially confusing.
+  // To use them, explicitly reference them, e.g. `window.name` or `window.status`.
+  "no-restricted-globals": [ "error", ...restrictedGlobals ],
+
+  // We are using the import plugin with its ordering capabilities
+  // for sorting declarations.
+  "sort-imports": [ "error", { ignoreDeclarationSort: true }],
+
+  // Activate our concepts of formatting imports
+  "import/order": [
+    "error",
+    {
+      groups: [ "builtin", "external", "unknown", "parent", "sibling", "index" ],
+      "newlines-between": "always"
+    }
+  ],
+
+  // Order with groups are confused by our module-resolver setup.
+  // It adds a newline after each import startinng with "-".
+  // At very least we like to have a newline between the import and the
+  // the actual implementation section.
+  "import/newline-after-import": "error",
+
+  // Nowadays we prefer not using default exports, but named ones
+  "import/default": "off",
+
+  // Fails with <FormattedNumber style="currency" />
+  "react/style-prop-object": "off",
+
+  // Don't allow dashes or underscores.
+  "filenames/match-regex": [ "error", "^[a-zA-Z][a-zA-Z0-9.]+$" ],
+
+  // Keep in sync with exported symbol name.
+  "filenames/match-exported": "error",
+
+  // Disable don't allow index.js files as this is used for exporting libs often times.
+  // Just be sure to not use them everywhere.
+  "filenames/no-index": "off",
+
   "@typescript-eslint/no-unused-vars": [
     "error",
     {
