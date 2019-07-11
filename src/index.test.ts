aed8ab96423071b9f5c6c48339cfef19b75ea95f
Sebastian Werner <s.werner@sebastian-software.de>
2019-07-11 12:04:55 +0200

    Fixed issues with running the unicorn plugin inside Jest

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,12 +2,20 @@ import { CLIEngine } from "eslint"
 
 const PATH_REPL = /"\S+(\\|\/)parser\.js"/
 
+function fixJestRequire() {
+  // Jest overrides this, but it breaks the "Unicorn" package.
+  // See also: https://github.com/sindresorhus/import-modules/issues/2
+  require.extensions = {
+    '.js': require('default-require-extensions/js')
+  }
+}
+
 function cloneWithSortedKeys(obj: any): any {
   const clone: { [key: string]: any } = {}
   const keys = Object.keys(obj).sort()
   keys.forEach((key) => {
     const value = obj[key]
-    if (typeof value === "object" && !(value instanceof Array)) {
+    if (typeof value === "object" && !Array.isArray(value)) {
       clone[key] = cloneWithSortedKeys(value)
     } else {
       clone[key] = value
@@ -22,7 +30,18 @@ function extractConfig(cli: CLIEngine, file: string) {
   return result.replace(new RegExp(PATH_REPL), '"[PATH]"')
 }
 
+test("Full configuration result", () => {
+  const cli = new CLIEngine({
+    useEslintrc: false,
+    configFile: "dist/index.js"
+  })
+
+  expect(extractConfig(cli, "src/index.js")).toMatchSnapshot()
+})
+
 test("load config in eslint to validate all rule syntax is correct", () => {
+  fixJestRequire()
+
   const cli = new CLIEngine({
     useEslintrc: false,
     configFile: "dist/index.js"
@@ -34,16 +53,9 @@ test("load config in eslint to validate all rule syntax is correct", () => {
   expect(result).toMatchSnapshot()
 })
 
-test("Full configuration result", () => {
-  const cli = new CLIEngine({
-    useEslintrc: false,
-    configFile: "dist/index.js"
-  })
-
-  expect(extractConfig(cli, "src/index.js")).toMatchSnapshot()
-})
-
 test("reports undeclared variable", () => {
+  fixJestRequire()
+
   const cli = new CLIEngine({
     useEslintrc: false,
     configFile: "dist/index.js"
