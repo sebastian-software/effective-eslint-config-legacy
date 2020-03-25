c08923918e47a2da1d0622506daa8b4c19baae34
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-25 18:06:26 +0100

    Enabled more nice unicorn rules. This package is really nice.

--- src/modules/unicorn.ts
+++ src/modules/unicorn.ts
@@ -1,28 +1,25 @@
 import { ESLintRules } from "../types"
 
 export const unicorn: ESLintRules = {
+  "unicorn/better-regex": "warn",
+  "unicorn/consistent-function-scoping": "warn",
+  "unicorn/custom-error-definition": "warn",
+  "unicorn/explicit-length-check": "warn",
+  "unicorn/filename-case": [ "warn", { cases: { camelCase: true, pascalCase: true } }],
+  "unicorn/import-index": "warn",
+  "unicorn/new-for-builtins": "error",
   "unicorn/no-array-instanceof": "error",
+  "unicorn/no-nested-ternary": "warn",
   "unicorn/no-new-buffer": "error",
-  "unicorn/no-unsafe-regex": "error",
+  "unicorn/no-unreadable-array-destructuring": "warn",
+  "unicorn/no-unsafe-regex": "warn",
   "unicorn/no-zero-fractions": "warn",
   "unicorn/prefer-includes": "warn",
+  "unicorn/prefer-replace-all": "warn",
+  "unicorn/prefer-spread": "warn",
   "unicorn/prefer-starts-ends-with": "warn",
-  "unicorn/regex-shorthand": "warn",
-  "unicorn/throw-new-error": "error",
-  "unicorn/consistent-function-scoping": "warn",
-  "unicorn/custom-error-definition": "error",
-  "unicorn/filename-case": [
-    "warn",
-    {
-      cases: {
-        camelCase: true,
-        pascalCase: true
-      }
-    }
-  ],
-  "unicorn/explicit-length-check": "warn",
-  "unicorn/new-for-builtins": "error",
-  "unicorn/no-nested-ternary": "warn",
-  "unicorn/prefer-type-error": "error",
-  "unicorn/prefer-string-slice": "warn"
+  "unicorn/prefer-string-slice": "warn",
+  "unicorn/prefer-type-error": "warn",
+  "unicorn/prevent-abbreviations": "warn",
+  "unicorn/throw-new-error": "error"
 }
