8c3c17a8f641958f3e96bb183d094459713a892b
Sebastian Werner <s.werner@sebastian-software.de>
2020-03-25 15:49:00 +0100

    Re-enabled import rules

--- src/modules/airbnb.ts
+++ src/modules/airbnb.ts
@@ -4,11 +4,11 @@ import nodeRules from 'eslint-config-airbnb-base/rules/node'
 // import styleRules from 'eslint-config-airbnb-base/rules/style'
 import varRules from 'eslint-config-airbnb-base/rules/variables'
 import es6Rules from 'eslint-config-airbnb-base/rules/es6'
-// import importRules from 'eslint-config-airbnb-base/rules/imports'
+import importRules from 'eslint-config-airbnb-base/rules/imports'
 import strictRules from 'eslint-config-airbnb-base/rules/strict'
 
 import { ESLintRules } from "../types"
-import { blacklist } from "../util"
+import { blacklist, isDisabled } from "../util"
 
 const unwantedValues = new Set(["eqeqeq", "array-callback-return", "no-restricted-globals"])
 
@@ -16,13 +16,13 @@ const basicallyUnchanged = new Set(["no-global-assign", "no-labels"])
 
 export const airbnb: ESLintRules = {}
 
-function merge(rules) {
+function merge(rules: ESLintRules): void {
   for (const name in rules) {
-    if (basicallyUnchanged.has(name) || unwantedValues.has(name)) {
+    if (blacklist.has(name) || basicallyUnchanged.has(name) || unwantedValues.has(name)) {
       return
     }
 
-    if (blacklist.has(name)) {
+    if (isDisabled(rules[name])) {
       return
     }
 
@@ -36,5 +36,5 @@ merge(nodeRules.rules)
 // merge(styleRules.rules)
 merge(varRules.rules)
 merge(es6Rules.rules)
-// merge(importRules.rules)
+merge(importRules.rules)
 merge(strictRules.rules)
--- src/util.ts
+++ src/util.ts
@@ -4,3 +4,8 @@ export const blockedByTS = Object.keys(configs["eslint-recommended"].overrides[0
 export const replacedByTS = Object.keys(configs.recommended.rules).filter((name) => !name.startsWith("@typescript-eslint"))
 
 export const blacklist = new Set([ ...blockedByTS, ...replacedByTS ])
+
+export function isDisabled(value) {
+  const toggle = value instanceof Array ? value[0] : value
+  return toggle === "off"
+}
