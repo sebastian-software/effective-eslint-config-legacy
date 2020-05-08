ccf638974c63c612915a84b51b88242ed56d76fd
Sebastian Werner <s.werner@sebastian-software.de>
2020-05-08 13:35:41 +0200

    Updated deps + added transform runtime as required now

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,8 @@ module.exports = {
   presets: [
     [ "@babel/env", { targets: { node: 8 } }],
     [ "@babel/preset-typescript", { isTSX: true, allExtensions: true }]
+  ],
+  plugins: [
+    "@babel/plugin-transform-runtime"
   ]
 }
