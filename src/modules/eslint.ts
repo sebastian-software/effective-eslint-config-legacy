9718b86a7193c2e49f7178a355860070ef533af9
Sebastian Werner <s.werner@sebastian-software.de>
2020-06-02 14:55:35 +0200

    Simplified code

--- src/modules/eslint.ts
+++ src/modules/eslint.ts
@@ -1,15 +1,7 @@
 import recommended from "eslint/conf/eslint-recommended"
 
 import { ESLintRules } from "../types"
-import { blacklist } from "../util"
+import { filterWithBlacklist } from "../util"
 
 // From eslint recommended and not yet alternatively implemented in TS preset
-export const eslint: ESLintRules = {}
-
-const allRules = recommended.rules as ESLintRules
-
-for (const name in allRules) {
-  if (!blacklist.has(name)) {
-    eslint[name] = allRules[name]
-  }
-}
+export const eslint: ESLintRules = filterWithBlacklist(recommended.rules)
