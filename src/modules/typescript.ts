d2c9dafb76fb8cfb7ea213f04e99625950f9c150
Sebastian Werner <s.werner@sebastian-software.de>
2020-06-02 12:50:26 +0200

    Simplied for now cleaned up TS module

--- src/modules/typescript.ts
+++ src/modules/typescript.ts
@@ -2,16 +2,7 @@ import { configs } from "@typescript-eslint/eslint-plugin"
 
 import { ESLintRules } from "../types"
 
-export const typescript: ESLintRules = {}
-
 const allRules = configs.recommended.rules
-
-const oldRuleNames = new Set([
-  "@typescript-eslint/camelcase",
-  "@typescript-eslint/class-name-casing",
-  "@typescript-eslint/interface-name-prefix",
-  "@typescript-eslint/ban-ts-ignore"
-])
 
 const newRules: ESLintRules = {
   // Via: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md#enforce-the-codebase-follows-eslints-camelcase-conventions
@@ -52,15 +43,7 @@ const newRules: ESLintRules = {
   "@typescript-eslint/ban-ts-comment": "warn"
 }
 
-for (const name in allRules) {
-  // "Funnily" the recommended preset lists deprecated rules
-  if (oldRuleNames.has(name)) {
-    continue
-  }
-
-  typescript[name] = allRules[name]
-}
-
-for (const name in newRules) {
-  typescript[name] = newRules[name]
+export const typescript: ESLintRules = {
+  ...allRules,
+  ...newRules
 }
