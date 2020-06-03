6b26615868a1dc52ee5d971d61f3eab93fda93c5
Sebastian Werner <s.werner@sebastian-software.de>
2020-06-03 14:39:52 +0200

    Exclude JS files from enhanced TS checks

--- src/index.ts
+++ src/index.ts
@@ -225,6 +225,20 @@ const config: ESLintConfig = {
         "no-undef": "off",
         "@typescript-eslint/no-unused-vars": "off"
       }
+    },
+
+    // Pure JavaScript files
+    {
+      // Definition files are typically really TS specific and
+      // do not work in the same way as normal TS files.
+      files: [ "*.js", "*.jsx", "*.mjs" ],
+      rules: {
+        // related to the 'any' type which cannot be validated for these files anyway
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/restrict-template-expressions": "off"
+      }
     }
   ]
 }
