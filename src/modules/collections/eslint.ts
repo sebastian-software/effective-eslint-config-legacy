6088c3cdcc897c6563424344ffb66d1baadeac05
Sebastian Werner <s.werner@sebastian-software.de>
2022-01-12 17:32:40 +0100

    Fixed import issues of recommended set from eslint

--- src/modules/collections/eslint.ts
+++ src/modules/collections/eslint.ts
@@ -1,6 +1,13 @@
-import recommended from "eslint/conf/eslint-recommended"
 import { Linter } from "eslint"
+import resolve from "enhanced-resolve"
 
+type ConfigData = {
+  rules: Linter.RulesRecord
+}
+
+const req = resolve.create.sync({ exportsFields:[] })
+
+const imported = req(__filename, "eslint/conf/eslint-recommended") as unknown as ConfigData
 
 // From eslint recommended and not yet alternatively implemented in TS preset
-export const eslint: Linter.RulesRecord = recommended.rules
+export const eslint: Linter.RulesRecord = { ...imported.rules }
