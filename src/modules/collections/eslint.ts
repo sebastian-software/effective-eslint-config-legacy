6e1115b527e0adebc57a7b2f663ba042e0db1be5
Sebastian Werner <s.werner@sebastian-software.de>
2022-01-13 09:40:39 +0100

    Improved loading of recommended rules

--- src/modules/collections/eslint.ts
+++ src/modules/collections/eslint.ts
@@ -1,19 +1,19 @@
-import { readFileSync } from "fs"
-
 import { Linter } from "eslint"
 import resolve from "enhanced-resolve"
 
 type ConfigData = {
-  rules?: Linter.RulesRecord
+  rules: Linter.RulesRecord
 }
 
 const req = resolve.create.sync({ exportsFields: [] })
-
 const resolved = req(__filename, "eslint/conf/eslint-recommended")
-const parsed: ConfigData = resolved
-  ? (JSON.parse(readFileSync(resolved, "utf-8")) as ConfigData)
-  : {}
-const rules: Linter.RulesRecord = parsed.rules || {}
+
+if (!resolved) {
+  throw new Error("Unable to load eslint:recommended preset!")
+}
+
+// eslint-disable-next-line import/no-dynamic-require, @typescript-eslint/no-var-requires
+const parsed: ConfigData = require(resolved)
 
 // From eslint recommended and not yet alternatively implemented in TS preset
-export const eslint: Linter.RulesRecord = rules
+export const eslint: Linter.RulesRecord = parsed.rules
