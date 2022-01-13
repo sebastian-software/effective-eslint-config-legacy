cebbfc9925d5d029f70a7fe0d3ba01b18aa4394e
Sebastian Werner <s.werner@sebastian-software.de>
2022-01-13 13:08:23 +0100

    Extracted rule compiler

---
+++ src/compiler.ts
@@ -0,0 +1,45 @@
+import { Linter } from "eslint"
+
+import { typescript } from "./modules/collections/typescript"
+import { eslint } from "./modules/collections/eslint"
+import { cra } from "./modules/collections/cra"
+import { airbnb } from "./modules/collections/airbnb"
+
+import { react } from "./modules/sandboxed/react"
+import { jsdoc } from "./modules/sandboxed/jsdoc"
+import { unicorn } from "./modules/sandboxed/unicorn"
+import { shopify } from "./modules/sandboxed/shopify"
+
+import { quality } from "./modules/quality"
+import { formatting } from "./modules/formatting"
+
+import { autofix } from "./modules/relax/autofix"
+import { relaxed } from "./modules/relax/relaxed"
+import { mergeLevelOverrides, mergeWithWarnings } from "./util"
+
+export function compileRules(): Linter.RulesRecord {
+  const combinedRules = {}
+
+  // plugin scope only
+  mergeWithWarnings(combinedRules, react, "react")
+  mergeWithWarnings(combinedRules, jsdoc, "jsdoc")
+  mergeWithWarnings(combinedRules, unicorn, "unicorn")
+  mergeWithWarnings(combinedRules, shopify, "shopify")
+
+  // popular collections
+  mergeWithWarnings(combinedRules, typescript, "typescript")
+  mergeWithWarnings(combinedRules, eslint, "eslint")
+  mergeWithWarnings(combinedRules, cra, "cra")
+  mergeWithWarnings(combinedRules, airbnb, "airbnb")
+
+  // local settings/overrides
+  mergeWithWarnings(combinedRules, quality, "quality", true)
+  mergeWithWarnings(combinedRules, formatting, "formatting", true)
+
+  // override/relax level
+  mergeLevelOverrides(combinedRules, autofix, "autofix")
+  mergeLevelOverrides(combinedRules, relaxed, "relaxed")
+
+  return combinedRules
+}
+
--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,11 @@ import {
   mergeWithWarnings,
   writeDefaultProjectConfig
 } from "./util"
-import { react } from "./modules/sandboxed/react"
 
-import { typescript } from "./modules/collections/typescript"
-import { eslint } from "./modules/collections/eslint"
-import { cra } from "./modules/collections/cra"
-import { airbnb } from "./modules/collections/airbnb"
-import { jsdoc } from "./modules/sandboxed/jsdoc"
-import { unicorn } from "./modules/sandboxed/unicorn"
-import { shopify } from "./modules/sandboxed/shopify"
-import { quality } from "./modules/quality"
-import { formatting } from "./modules/formatting"
-import { autofix } from "./modules/relax/autofix"
-import { relaxed } from "./modules/relax/relaxed"
 import { jestOverride } from "./overrides/jest"
 import { jsOverride } from "./overrides/javascript"
 import { typedefOverride } from "./overrides/typedef"
+import { compileRules } from "./compiler"
 
 const ROOT = String(appRootPath)
 let projectConfig = findUp.sync([ "tsconfig.json", "jsconfig.json", "package.json" ], {
@@ -47,28 +36,6 @@ if (projectConfig == null) {
     writeDefaultProjectConfig(projectConfig)
   }
 }
-
-const combinedRules: Linter.RulesRecord = {}
-
-// plugin scope only
-mergeWithWarnings(combinedRules, react, "react")
-mergeWithWarnings(combinedRules, jsdoc, "jsdoc")
-mergeWithWarnings(combinedRules, unicorn, "unicorn")
-mergeWithWarnings(combinedRules, shopify, "shopify")
-
-// popular collections
-mergeWithWarnings(combinedRules, typescript, "typescript")
-mergeWithWarnings(combinedRules, eslint, "eslint")
-mergeWithWarnings(combinedRules, cra, "cra")
-mergeWithWarnings(combinedRules, airbnb, "airbnb")
-
-// local settings/overrides
-mergeWithWarnings(combinedRules, quality, "quality", true)
-mergeWithWarnings(combinedRules, formatting, "formatting", true)
-
-// override/relax level
-mergeLevelOverrides(combinedRules, autofix, "autofix")
-mergeLevelOverrides(combinedRules, relaxed, "relaxed")
 
 const EXTENSIONS = [ ".js", ".jsx", ".d.ts", ".ts", ".tsx", ".mjs", ".json" ]
 
@@ -123,7 +90,7 @@ const config: Linter.BaseConfig = {
     "filenames"
   ],
 
-  rules: combinedRules,
+  rules: compileRules(),
 
   overrides: [
     jestOverride,
